d983957200f9fec791e0e9c9b94abb4413f28e87
Sandesh-25 <sandeshgome2505@gmail.com>
2025-05-01 12:48:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const sheetID = "YOUR_SHEET_ID";
 const sheetName = "Sheet1";
-const url = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?tqx=out:json&sheet=${sheetName}`;
+const url = `https://docs.google.com/spreadsheets/d/e/2PACX-1vSl2X5iANv--GC5dh3Q_4UzZoGfqe-SjcFfUFijPfejLWlknVRnqkyNyov2Oky5HApBtCNlTdIN3wXC/pub?output=csv`;
 
 let fullData = [];
 
